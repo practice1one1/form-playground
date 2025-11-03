3515cbd404922347ab22395899eada311d353f7d
practice1one1 <practice1one1@gmail.com>
2025-11-03 07:48:41 +0000

    fix: configure Vite dev server to host files from dynamic domains of codesandbox

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["fcjzd2-5173.csb.app"],
+  }
 });
