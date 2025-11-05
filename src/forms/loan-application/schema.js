d2dd10320886749bccccb6d6fbc8002f455e394c
Rinidh <rinidhdhokia@gmail.com>
2025-11-05 21:00:53 +0300

    fix(validation): define error messages with `error` prop in params object, use better `phone` validation and write more specific custom error messages in other fields

--- src/forms/loan-application/schema.js
+++ src/forms/loan-application/schema.js
@@ -1,13 +1,12 @@
 import z from "zod";
 
-// ------------------ Zod Schema ------------------
 export const loanSchema = z.object({
   loanAmount: z
-    .number({ invalid_type_error: "Enter a valid amount" })
+    .number({ error: "Enter a valid amount" })
     .positive("Must be greater than 0"),
   annualIncome: z
-    .number({ invalid_type_error: "Enter a valid income" })
-    .positive(),
+    .number({ error: "Enter a valid income" })
+    .positive("Must be greater than 0 for you to qualify"),
   bankStatement: z
     .custom((files) => files && files.length > 0, "Bank statement is required")
     .refine(
@@ -18,27 +17,34 @@ export const loanSchema = z.object({
     ),
   loanPurpose: z.string().min(1, "Select a loan purpose"),
   otherPurpose: z.string().optional(),
-  title: z.string().min(1, "Required"),
+  title: z.string().min(1, "Please enter your title e.g. Mr., Mrs., Dr. etc."),
   firstName: z.string().min(2, "Too short"),
   lastName: z.string().min(2, "Too short"),
   maritalStatus: z.string().min(1, "Select marital status"),
-  email: z.string().email("Invalid email"),
-  phone: z.string().min(7, "Invalid phone number"),
+  email: z.email("Invalid email"),
+  phone: z
+    .string()
+    .trim()
+    .regex(
+      /^\+?[0-9\s\-()]{5,20}$/, // smallest number with min 4 phone number digits + 1 country code digit. Largest number with 17 phone number digits + 3 country code digits
+      "Please enter a valid phone number including country code"
+    ),
   street: z.string().min(1, "Required"),
   city: z.string().min(1, "Required"),
   state: z.string().min(1, "Required"),
   postalCode: z.string().min(3, "Invalid postal/ZIP"),
-  occupation: z.string().min(1, "Required"),
-  employerFirstName: z.string().min(1, "Required"),
-  employerLastName: z.string().min(1, "Required"),
-  companyName: z.string().min(1, "Required"),
-  yearsExperience: z
-    .number({ invalid_type_error: "Enter a valid number" })
-    .min(0),
-  grossMonthlyIncome: z
-    .number({ invalid_type_error: "Enter a valid amount" })
-    .min(0),
-  monthlyRent: z.number({ invalid_type_error: "Enter a valid amount" }).min(0),
-  downPayment: z.number({ invalid_type_error: "Enter a valid amount" }).min(0),
+  occupation: z.string().min(1, "Please enter a descriptive occupation name"),
+  employerFirstName: z
+    .string()
+    .min(
+      1,
+      "Employer name is required (Write your own compnay name if you are self-employed)"
+    ),
+  employerLastName: z.string().optional(),
+  companyName: z.string().min(1, "Current company name is required"),
+  yearsExperience: z.number({ error: "Enter a valid number of years" }).min(0),
+  grossMonthlyIncome: z.number({ error: "Enter a valid amount" }).min(0),
+  monthlyRent: z.number({ error: "Enter a valid amount" }).min(0),
+  downPayment: z.number({ error: "Enter a valid amount" }).min(0),
   comments: z.string().optional(),
 });
